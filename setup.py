351580821f2e0a6f3e92dd6703eec8f725d273ee
Ziaeemehr <a.ziaeemehr@gmail.com>
2021-08-21 22:09:23 +0430

    app_HH modified.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name='brian_dash',
-    version='0.0.1',
+    version='0.1',
     author="Abolfazl Ziaeemehr",
     author_email="a.ziaeemehr@gmail.com",
     description="Dashboard for neuron simulator Brian2",
